814fcbad79a078c33bb8642d7c2ea43c6276142d
matildeopbravo <matildeopbravo@gmail.com>
2020-03-21 22:52:06 +0000

    alterei sintaxe da funcao inicializar_estado

--- src/dados.c
+++ src/dados.c
@@ -6,14 +6,18 @@
 ESTADO * inicializar_estado(){
 
     // calloc coloca todos os 'pedacos' de memória a 0, no caso do tabuleiro, 0 = VAZIO. Só resta colocar a peca Branca.
-    ESTADO *e = calloc(1, sizeof(ESTADO)); 
-    e->jogador_atual = 1;
-    e->num_jogadas = 0;
-    e->ultima_jogada = (COORDENADA) {3,4};
-    e->tab[3][4] = BRANCA;
-    e->tab[0][7] = DOIS;
-    e->tab[7][0] = UM;
-	return e ;	
+    ESTADO *e = calloc(1, sizeof(ESTADO));
+  
+    *e =(ESTADO) {
+ 
+    .jogador_atual = 1,
+    .num_jogadas = 0,
+    .ultima_jogada = (COORDENADA) {3,4},
+    .tab[3][4] = BRANCA,
+    .tab[0][7] = DOIS,
+    .tab[7][0] = UM };
+
+    return e ;	
 }   
 
 int obter_jogador_atual (ESTADO *estado) {
